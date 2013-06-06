dfc67fcc8d833b408fbccb519dccacbe909f3bf0
schulzh <schulzh82@gmail.com>
2013-06-06 15:27:59 +0200

    parameters corrected

--- model_creator.py
+++ model_creator.py
@@ -1,13 +1,11 @@
 import argparse
 import subprocess
 import shared
-
-"""
+import sys
 
 parser = argparse.ArgumentParser(description='ProteinPrediction by Group 23 \n\
 \n\
 given c and gamma values, this program creates a model file for later predictions.')
-parser.add_argument('-p', metavar='wekapath', dest='WEKA_PATH', required=True, help='points to the weka executable e.g. /mnt/opt/data/pp1_13_exercise/weka-3-6-9/weka.jar')
 parser.add_argument('-c', metavar='Value c', dest='cValue', required=True, help='c value')
 parser.add_argument('-g', metavar='Value gamma', dest='gammaValue', required=True, help='gamma value')
 parser.add_argument('-a', metavar='arff file', dest='arff', required=True, help='the arf file c and gamma is calculated for')
@@ -23,18 +21,13 @@ try:
 except IOError:
    print 'Arff file not found!'
    sys.exit(1)
-try:
-   with open(my.model): pass
-except IOError:
-   print 'Model file already exists!'
-   sys.exit(1)
 
 
 """
 
-c = 1
-gamma = 1
-train_in = "tmps_0.arff"
+c = args.cValue
+gamma = args.gammaValue
+train_in = args.arff
 
 print "build a new model ..."
 print "filter files first ..."
@@ -44,3 +37,4 @@ print "files filterd! build model ..."
 print subprocess.check_output(shared.store_model_command(filtered_file, shared.MODEL_NAME, c, gamma))
 print "done!"
 
+"""
--- predictor.py
+++ predictor.py
@@ -4,37 +4,24 @@ import sys
 import subprocess
 
 
-"""
 parser = argparse.ArgumentParser(description='ProteinPrediction by Group 23 \n\
 \n\
 prediction program')
-parser.add_argument('-p', metavar='wekapath', dest='WEKA_PATH', required=True, help='points to the weka executable e.g. /mnt/opt/data/pp1_13_exercise/weka-3-6-9/weka.jar')
 parser.add_argument('-a', metavar='arff file', dest='arff', required=True, help='arff file for predictions')
 args = parser.parse_args()
-"""
 
-input_file = "tmps_independent.arff"
+input_file = args.arff
 
-try:
-   with open(args.WEKA_PATH): pass
-except IOError:
-   print 'Path to weka is invalid!'
-   sys.exit(1)
 try:
    with open(args.arff): pass
 except IOError:
    print 'Specified arff file does not exist!'
    sys.exit(1)
 try:
-   with open(my.model): pass
+   with open(shared.MODEL_NAME): pass
 except IOError:
-   print 'Model file does not exist! Please run model_creator first.'
+   print "Create a model first. Use the model_creator.py!"
    sys.exit(1)
-try:
-    with open(shared.MODEL_NAME): pass
-except IOError:
-    print "Create a model first. Use the model_creator.py!"
-    sys.exit(1)
     
 
 filtered_file = "pred_filtered.arff"
